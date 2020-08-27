f7471d557bbd21f00fbd27e99af76bdf58bac828
samga <samgalaxy111111@gmail.com>
2020-08-27 11:08:42 +0700

    add Fragment in Activity

--- app/src/main/java/id/sam/myflexiblefragment/MainActivity.java
+++ app/src/main/java/id/sam/myflexiblefragment/MainActivity.java
@@ -1,8 +1,11 @@
 package id.sam.myflexiblefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        HomeFragment mHomeFragment = new HomeFragment();
+        Fragment fragment = mFragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName());
+
+        if (!(fragment instanceof HomeFragment)){
+            mFragmentManager
+                    .beginTransaction()
+                    .add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName())
+                    .commit();
+        }
     }
 }
